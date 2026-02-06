72f23d9f7fd206bacbe4d4ad6b67d6bc2f2e69a1
Hasim <hasimuener@gmail.com>
2026-02-06 22:59:52 +0000

    4

--- functions.php
+++ functions.php
@@ -1,5 +1,13 @@
 <?php
 add_action( 'wp_enqueue_scripts', 'hp_journal_enqueue_styles' );
 function hp_journal_enqueue_styles() {
+    // Lädt das Eltern-Theme
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    
+    // Lädt dein Child-Theme mit einem Zeitstempel (Cache-Buster)
+    wp_enqueue_style( 'child-style', 
+        get_stylesheet_directory_uri() . '/style.css', 
+        array('parent-style'), 
+        time() // Das hier zwingt den Browser zum Neuladen!
+    );
 }
