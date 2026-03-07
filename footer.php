7ba97a688d74b09a407677730e06411c605590ef
domdar <hasimster@gmail.com>
2026-03-07 01:39:13 +0100

    Külüm

--- footer.php
+++ footer.php
@@ -65,11 +65,8 @@ $hp_year    = gmdate( 'Y' );
                 </ul>
             </div>
 
-            <div class="hp-colophon__slogan" aria-label="Leitsatz">
-                <p>Wer die Ordnung versteht, muss sie nicht hinnehmen.</p>
-            </div>
-
         </div><!-- .hp-colophon__inner -->
+        <p class="hp-colophon__closing">Wer die Ordnung versteht, muss sie nicht hinnehmen.</p>
     </footer>
 
 </div><!-- .site (GeneratePress) -->
